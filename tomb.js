55a99a6830162beb3178dbf4cd47dad4aaa7457e
barbyns <barbyns95@gmail.com>
2025-02-20 16:03:28 +0100

    Non funziona il css

---
+++ tomb.js
@@ -0,0 +1,23 @@
+let numeriEstratti = new Set ();
+let tabellone = document.getElementById("tabellone");
+ for (let i=1; i <= 76; i++ ){
+    let cella = document.createElement("div")
+    cella.classList.add("numero");
+    cella.textContent= i;
+    cella.id= "num-" + i;
+    tabellone.appendChild(cella);
+ }
+
+function estraiNumero () {
+    if(numeriEstratti.size >=76){
+        alert("tutti i numeri sono stati estratti!");
+        return;
+    }
+    let numero;
+    do {
+        numero = Math.floor(Math.random()* 76)+1;
+    }  while (numeriEstratti.has(numero));
+numeriEstratti.add(numero);
+document.getElementById("numero-estratto").textContent = numero;
+documento.getElementById("num-" + numero).classList.add("estratto");
+}
